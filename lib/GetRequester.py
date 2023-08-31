0c2c35a6b83abe235df6b6f5e9f60ddb4376b3bb
Red <red.sarouf2014@gmail.com>
2023-08-30 21:13:52 -0400

    add python p4 getting remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data = []
+        json_data = json.loads(self.get_response_body())
+        for d in json_data:
+            data.append(d)
+        return data
+
+  
